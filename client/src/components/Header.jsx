c9769a85c9c32d003e8b86abcbc52bbf246fd854
Ashikms1998 <142452260+Ashikms1998@users.noreply.github.com>
2025-06-04 23:24:51 +0530

    created header section

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,64 @@
+import {
+  Button,
+  Navbar,
+  TextInput,
+  NavbarCollapse,
+  NavbarLink,
+  NavbarToggle,
+} from "flowbite-react";
+import React from "react";
+import { AiOutlineSearch } from "react-icons/ai";
+import { FaMoon } from "react-icons/fa";
+import { Link, useLocation } from "react-router-dom";
+
+export default function Header() {
+  const location = useLocation();
+  const path = location.pathname;
+  return (
+    <Navbar className=" border-b-2 ">
+      <Link
+        to="/"
+        className="self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white"
+      >
+        <span className="px-2 py-1 bg-gradient-to-r from-indigo-800 via-purple-500 to-pink-500 rounded-lg text-white">
+          Ashik's Blog
+        </span>
+      </Link>
+      <form>
+        <TextInput
+          type="text"
+          placeholder="Search..."
+          rightIcon={AiOutlineSearch}
+        />
+      </form>
+      <div className="flex gap-2 md:order-2">
+        <Button className="w-22 h-10 hidden sm:inline" color="gray" pill>
+          <FaMoon className="fill-black" />
+        </Button>
+        <Link to="/sign-in">
+          <Button
+            color="purple"
+            className="bg-gradient-to-r from-purple-500 via-indigo-500 to-blue-500 text-white hover:opacity-90 transition-all"
+          >
+            Sign In
+          </Button>
+          <NavbarToggle color="gray" />
+        </Link>
+      </div>
+      <NavbarCollapse>
+        <NavbarLink active={path === "/about"} as={'div'}>
+          <Link to="/about">About</Link>
+        </NavbarLink>
+        <NavbarLink active={path === "/"} as={'div'}>
+          <Link to="/">Home</Link>
+        </NavbarLink>
+        <NavbarLink active={path === "/sign-in"} as={'div'}>
+          <Link to="/sign-in">Sign In</Link>
+        </NavbarLink>
+        <NavbarLink active={path === "/sign-up"} as={'div'}>
+          <Link to="/sign-up">Sign Up</Link>
+        </NavbarLink>
+      </NavbarCollapse>
+    </Navbar>
+  );
+}
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,11 +1,15 @@
+const flowbiteReact = require("flowbite-react/plugin/tailwindcss");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+    'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}',
+    "node_modules/flowbite/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require('flowbite/plugin')],
 };
