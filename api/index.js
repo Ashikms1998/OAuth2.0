e53e7a8ee8ee1b4c286b1526bb20aa89d2a37ba2
Ashikms1998 <142452260+Ashikms1998@users.noreply.github.com>
2025-06-18 16:01:53 +0530

    created a test api route

--- api/index.js
+++ api/index.js
@@ -1,19 +1,23 @@
-import express from 'express'
-import mongoose from 'mongoose'
-import dotenv from 'dotenv'
-dotenv.config()
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import userRoutes from "./routes/user.routes.js";
+dotenv.config();
 
-mongoose.connect(process.env.MONGO_URL)
-.then(()=>{
-    console.log("MongoDB is connected")
-}).catch((error)=>{
-    console.log("Error connecting MongoDB",error)
-})
-
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log("MongoDB is connected");
+  })
+  .catch((error) => {
+    console.log("Error connecting MongoDB", error);
+  });
 
 const app = express();
 
+app.listen(3000, () => {
+  console.log("Server is running");
+});
 
-app.listen(3000,()=>{
-    console.log("Server is running")
-})
+app.use("/api/user", userRoutes);
+ 
